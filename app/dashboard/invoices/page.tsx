9d80d27c45a36f3d0962da88a1322a2a5f0c8c7f
saulyez <saulye25@gmail.com>
2025-06-19 20:22:12 +0100

    Fixed: typo

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -8,12 +8,12 @@ import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
  
 export default async function Page(props: {
-    searchParam?: Promise <{
+    searchParams?: Promise <{
         query?: string;
         page?: string
     }>;
 }) {
-    const searchParams = await props.searchParam;
+    const searchParams = await props.searchParams;
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
     const totalPages = await fetchInvoicesPages(query);
