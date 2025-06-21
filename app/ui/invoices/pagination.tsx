66a3d7d0603b3a997173f0f969d60cb7d0f86e1d
saulyez <saulye25@gmail.com>
2025-06-21 21:02:59 +0100

    Added linting and accessibility. Also server form validation

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -11,7 +11,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const pathName = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
-  // const allPages = generatePagination(currentPage, totalPages);
+  const allPages = generatePagination(currentPage, totalPages);
 
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
@@ -20,9 +20,8 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   }
   return (
     <>
-      {/*  NOTE: Uncomment this code in Chapter 11 */}
-
-      {/* <div className="inline-flex">
+      
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -55,7 +54,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
